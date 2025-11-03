c3e93dbfbe283f9b5e2da6c30b352abdee8c30ad
anand1302-byte <anandjethava02@gmail.com>
2025-11-04 01:57:02 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  reactStrictMode: true,
   images: {
-    unoptimized: true,
+    domains: [],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
+
